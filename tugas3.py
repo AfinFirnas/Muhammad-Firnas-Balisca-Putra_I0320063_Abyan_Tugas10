fdb7542acf0068632e69e38bd8902d5a5b019ed4
AfinFirnas <80524706+AfinFirnas@users.noreply.github.com>
2021-05-07 17:58:32 +0700

    Add files via upload

---
+++ tugas3.py
@@ -0,0 +1,19 @@
+print('Selamat Datang Di Program Biodata')
+print('=================================')
+
+# Ambil Input Dari User
+nama = input('Masukkan Nama : ')
+umur = input('Masukkan Umur : ')
+alamat = input('Masukkan Alamat : ')
+
+# Format Teks
+teks = 'Nama: {}\n umur: {}\n Alamat: {}\n'.format(nama,umur,alamat)
+
+# Buka File Untuk Ditulis
+file_bio = open('file.txt','a')
+
+# Tulis Teks Ke File
+file_bio.write(teks)
+
+# Tutup File
+file_bio.close()
